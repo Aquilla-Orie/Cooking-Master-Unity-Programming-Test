73ff31e90d0f21795c61f34497cba0dce43949fa
Chukwu Gabriel <44025156+Aquilla-Orie@users.noreply.github.com>
2022-05-04 17:00:18 +0100

    Update Camera Logic
    Camera that pans in/out depending on the distance between players in the level.

---
+++ Assets/Scripts/Camera/GameCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+    [SerializeField] private Transform _player1;
+    [SerializeField] private Transform _player2;
+    [SerializeField] private Camera _cam;
+
+    private void Update()
+    {
+        float playerDistance = Vector2.Distance(_player1.position, _player2.position);
+        _cam.orthographicSize = Mathf.Clamp(playerDistance, 4, 6);
+    }
+}
