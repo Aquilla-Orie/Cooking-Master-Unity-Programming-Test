2bb5c2fd40f1939f4e45655f4406c1fafe5811ca
Chukwu Gabriel <44025156+Aquilla-Orie@users.noreply.github.com>
2022-05-04 17:02:43 +0100

    Update GameCamera.cs

--- Assets/Scripts/Camera/GameCamera.cs
+++ Assets/Scripts/Camera/GameCamera.cs
@@ -8,9 +8,13 @@ public class GameCamera : MonoBehaviour
     [SerializeField] private Transform _player2;
     [SerializeField] private Camera _cam;
 
+    [SerializeField] private float _minOrthographicSize = 4;
+    [SerializeField] private float _maxOrthographicSize = 6;
+
     private void Update()
     {
+        //Pan camera based off distance between the two players
         float playerDistance = Vector2.Distance(_player1.position, _player2.position);
-        _cam.orthographicSize = Mathf.Clamp(playerDistance, 4, 6);
+        _cam.orthographicSize = Mathf.Clamp(playerDistance, _minOrthographicSize, _maxOrthographicSize);
     }
 }
